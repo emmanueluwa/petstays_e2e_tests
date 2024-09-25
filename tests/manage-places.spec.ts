d85fb2cb55b060a56ad121bf1ae7e0dee7cb60c1
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-25 08:12:01 +0100

    adding test for user should be allowed to add a place

---
+++ tests/manage-places.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from "@playwright/test";
+import path from "path";
+
+const UI_URL = "http://localhost:5173";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(`${UI_URL}/add-place`);
+
+  //get the login button
+  await page.getByRole("link", { name: "Login" }).click();
+
+  //confirm "Login" text is visible - proof of being on login page
+  await expect(page.getByRole("heading", { name: "Login" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("ten@ten.com");
+  await page.locator("[name=password]").fill("password123");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  //visuals that confirm we have logged in
+  await expect(page.getByText("Login successful")).toBeVisible(); //toast
+});
+
+test("should allow user to add a place", async ({ page }) => {
+  await page.goto(`${UI_URL}/add-place`);
+
+  await page.locator('[name="name"]').fill("Test Place");
+  await page.locator('[name="city"]').fill("Test City");
+  await page.locator('[name="country"]').fill("Test Country");
+  await page
+    .locator('[name="description"]')
+    .fill("Test testing the description section for the test place");
+  await page.locator('[name="pricePerNight"]').fill("89");
+  await page.locator('[name="pricePerNight"]').fill("89");
+
+  await page.selectOption('select[name="starRating"]', "4");
+  await page.getByText("Penthouse").click();
+  await page.getByLabel("Free Wifi").check();
+  await page.getByLabel("Parking").check();
+
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("6");
+
+  await page.setInputFiles("[name='imageFiles']", [
+    path.join(__dirname, "files", "ein.jpg"),
+    path.join(__dirname, "files", "zwei.jpg"),
+  ]);
+
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Place saved")).toBeVisible();
+});
